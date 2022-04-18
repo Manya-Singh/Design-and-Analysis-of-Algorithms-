f08a2b8cb80bd256c4245e3abfd3a4372bd173d4
Manya Singh <77196311+Manya-Singh@users.noreply.github.com>
2022-04-18 15:29:18 +0530

    Update Key=Sum.cpp

--- Week-5/Key=Sum/Key=Sum.cpp
+++ Week-5/Key=Sum/Key=Sum.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n,i,t,key;
+    string separator=""; //none at first
+    cin>>t; //Total number of test cases
+    while(t--){
+        cin>>n; //Size of array
+        int arr[n]; 
+        //Elements of array
+        for(i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cin>>key;
+        sort(arr,arr+n);
+        int l=0,r=n-1,f=0;
+        while(l<r){
+            if(arr[l]+arr[r]==key){
+                cout<<separator<<arr[l]<<" "<<arr[r];
+                separator=", "; //comma after first
+                r--;
+                f=1;
+            }
+            else if (arr[l]+arr[r]<key)
+                l++;
+            else
+                r--;
+        }
+        if(f==0)
+            cout<<"\nNo such pair exist"<<endl;
+    }
+    return 0;
+}
